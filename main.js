a809c0179372a62d9338188545599ce19c2969c3
Antonio Fry <antoniofry2000@gmail.com>
2019-04-12 14:10:05 -0600

    change color to match comp and edit js function

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-Phase 1
-	Creating new To-dos:
-		- Should be able to add new tasks using the add item form
-			- Query the input and button within the form
-			- create a function that gets the input
+// Phase 1
+// 	Creating new To-dos:
+// 		- Should be able to add new tasks using the add item form
+// 			- Query the input and button within the form
+// 			- create a function that gets the input
 
 
 
 var addTaskInput = document.querySelector('#task-item-input');
 var addTaskBtn = document.querySelector('#add-item-btn');
+var previewedTasks = document.querySelector('#preview-tasks')
 
 addTaskBtn.addEventListener('click', addTask);
 
 
-addTask() {
+function addTask(e) {
 	var asideTasks = [];
+	if (addTaskInput.value === '') {
+		return alert('Please enter a task item.');
+	}
 	asideTasks.push(addTaskInput.value);
-	
+	asideTasks.map
 }
