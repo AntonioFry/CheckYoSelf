c3215e6e17b96c2cdcf68e0fb6cdf5a967db1c47
Antonio Fry <antoniofry2000@gmail.com>
2019-04-12 11:03:36 -0600

    add function to add tasks

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 Phase 1
+	Creating new To-dos:
+		- Should be able to add new tasks using the add item form
+			- Query the input and button within the form
+			- create a function that gets the input
+
+
+
+var addTaskInput = document.querySelector('#task-item-input');
+var addTaskBtn = document.querySelector('#add-item-btn');
+
+addTaskBtn.addEventListener('click', addTask);
+
+
+addTask() {
+	var asideTasks = [];
+	asideTasks.push(addTaskInput.value);
+	
+}
